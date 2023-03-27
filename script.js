00aaf5c72233cebc4b15ad50e94e4262ee21dab5
ornel77 <t.ornel@yahoo.fr>
2023-03-27 22:21:41 +0200

    level1 terminé

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* ------------------------------- GET ELEMENT ------------------------------ */
+
+const form = document.querySelector('form')
+const list = document.querySelector('.todo-list')
+const input = document.querySelector('.todo-input')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const item = document.createElement('li')
+    item.classList.add('todo-item')
+    item.innerHTML = input.value
+    list.append(item)
+    input.value = ''
+})
